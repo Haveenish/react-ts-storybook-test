07c8ef61efe1f581a23b893de965a8f356ccbf1d
Haveenish <haveenish.s@gmail.com>
2024-06-20 23:53:35 +0800

    Create TextField story, Remove storybook boilerplate comments from Button story

---
+++ src/stories/TextField.stories.ts
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import TextField from "../components/TextField";
+
+const meta = {
+  title: "Components/TextField",
+  component: TextField,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: { onChange: fn() },
+} satisfies Meta<typeof TextField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: "Input text",
+    value: "Test",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: "large",
+    label: "Input text",
+    value: "Test",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: "small",
+    label: "Input text",
+    value: "Test",
+  },
+};
